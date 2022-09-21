4c23c408e16e7d151962f305d8984389dca9972e
Tim Allclair <tallclair@google.com>
2022-09-21 09:39:11 -0700

    WIP: finish grpc_tunnle basics

--- pkg/server/tunnel/grpc_tunnel.go
+++ pkg/server/tunnel/grpc_tunnel.go
@@ -17,12 +17,13 @@ limitations under the License.
 package tunnel
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 	"sync/atomic"
 	"time"
 
-	"github.com/go-logr/logr"
+	"github.com/google/uuid"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/apiserver-network-proxy/konnectivity-client/proto/client"
 )
@@ -44,10 +45,12 @@ type grpcTunnel struct {
 	backend   connection
 	startTime time.Time
 
-	agentID string
-	address string
-	_dialID int64
+	logger klog.Logger
 
+	agentID         string
+	address         string
+	_dialID         int64
+	tunnelUID       string // tunnel UID is generated on tunnel creation, and used for tracing logs
 	atomicConnectID int64
 
 	// stateMu guards the tunnel and connection states
@@ -107,18 +110,22 @@ func (s ConnectionState) String() string {
 }
 
 func NewGrpcTunnel(frontend, backend GrpcClient, agentID string, dialID int64, address string) Tunnel {
+	uid := uuid.New().String()
+	logger := klog.NewKlogr().WithValues("tunnelUID", uid)
 	return &grpcTunnel{
 		frontend: connection{
 			GrpcClient: frontend,
-			logger:     klog.NewKlogr().WithName("frontend"),
+			logger:     logger.WithName("frontend"),
 		},
 		backend: connection{
 			GrpcClient: backend,
-			logger:     klog.NewKlogr().WithName("backend").WithValues("agentID", agentID),
+			logger:     logger.WithName("backend").WithValues("agentID", agentID),
 		},
+		logger:    logger,
 		_dialID:   dialID,
 		agentID:   agentID,
 		startTime: time.Now(),
+		tunnelUID: uid,
 	}
 }
 
@@ -126,71 +133,170 @@ func (t *grpcTunnel) RecvFrontend(pkt *client.Packet) {
 	switch pkt.Type {
 	case client.PacketType_DIAL_REQ:
 		if !t.transitionState(TunnelUninitialized, TunnelDialing) {
-			klog.ErrorS(nil, "DIAL_REQ received for tunnel in wrong state", "state", t.state)
+			t.logger.Error(nil, "DIAL_REQ received for tunnel in wrong state", "state", t.state)
 			return
 		}
 		t.frontend.setState(ConnectionDialing)
 		t.backend.setState(ConnectionDialing)
 		if err := t.backend.Send(pkt); err != nil {
-			klog.ErrorS(err, "DIAL_REQ to Backend failed", "dialID", t.dialID())
+			t.logger.Error(err, "DIAL_REQ to Backend failed", "dialID", t.dialID())
 			t.BackendClosed() // Initial dial failed, treat the backend as closed.
 		} else {
-			klog.V(5).InfoS("DIAL_REQ sent to backend", "dialID", t.dialID(), "agentID", t.agentID)
-		}
-
-		////// HEEEEEEEEEEERE
-
-	case client.PacketType_CLOSE_REQ:
-		connID := pkt.GetCloseRequest().ConnectID
-		klog.V(5).InfoS("Received CLOSE_REQ", "connectionID", connID)
-		if backend == nil {
-			klog.V(2).InfoS("Backend has not been initialized for requested connection. Client should send a Dial Request first",
-				"serverID", s.serverID, "connectionID", connID)
-			continue
-		}
-		if err := backend.Send(pkt); err != nil {
-			// TODO: retry with other backends connecting to this agent.
-			klog.ErrorS(err, "CLOSE_REQ to Backend failed", "serverID", s.serverID, "connectionID", connID)
-		} else {
-			klog.V(5).InfoS("CLOSE_REQ sent to backend", "serverID", s.serverID, "connectionID", connID)
+			t.logger.V(5).Info("DIAL_REQ sent to backend", "dialID", t.dialID(), "agentID", t.agentID)
 		}
 
 	case client.PacketType_DIAL_CLS:
-		random := pkt.GetCloseDial().Random
-		klog.V(5).InfoS("Received DIAL_CLOSE", "serverID", s.serverID, "dialID", random)
-		// Currently not worrying about backend as we do not have an established connection,
-		s.PendingDial.Remove(random)
-		klog.V(5).InfoS("Removing pending dial request", "serverID", s.serverID, "dialID", random)
+		if s := t.getState(); s != TunnelDialing {
+			t.logger.Error(nil, "Received DIAL_CLS from frontend in unexpected state. Proceeding to shutdown anyway.",
+				"state", s,
+				"dialID", t.dialID())
+		}
+		t.setState(TunnelShutdown)
+		t.frontend.setState(ConnectionClosing)
+		t.backend.close()
+
+	case client.PacketType_CLOSE_REQ:
+		if s := t.getState(); s != TunnelConnected {
+			t.logger.Error(nil, "Received CLOSE_REQ from frontend in unexpected state. Proceeding to shutdown anyway.",
+				"state", s,
+				"connectID", t.connectID())
+		}
+		t.setState(TunnelShutdown)
+		t.frontend.setState(ConnectionClosing)
+		t.backend.setState(ConnectionClosing)
+		t.backend.Send(pkt)
 
 	case client.PacketType_DATA:
-		connID := pkt.GetData().ConnectID
-		data := pkt.GetData().Data
-		klog.V(5).InfoS("Received data from connection", "bytes", len(data), "connectionID", connID)
-		if firstConnID == 0 {
-			firstConnID = connID
-		} else if firstConnID != connID {
-			klog.V(5).InfoS("Data does not match first connection id", "fistConnectionID", firstConnID, "connectionID", connID)
+		if s := t.getState(); s != TunnelConnected {
+			t.logger.V(5).Info("Ignoring DATA packet from frontend: invalid state.",
+				"state", s,
+				"connectID", t.connectID())
+			return
+		}
+		if err := t.backend.Send(pkt); err != nil {
+			t.logger.Error(err, "Failed to deliver DATA packet to backend",
+				"connectID", t.connectID(),
+				"agentID", t.agentID)
 		}
 
-		if backend == nil {
-			klog.V(2).InfoS("Backend has not been initialized for the connection. Client should send a Dial Request first", "connectionID", connID)
-			continue
-		}
-		if err := backend.Send(pkt); err != nil {
-			// TODO: retry with other backends connecting to this agent.
-			klog.ErrorS(err, "DATA to Backend failed", "serverID", s.serverID, "connectionID", connID)
-			continue
-		}
-		klog.V(5).Infoln("DATA sent to Backend")
+	case client.PacketType_DIAL_RSP, client.PacketType_CLOSE_RSP:
+		t.logger.V(2).Info("Invalid packet from frontend", "type", pkt.Type.String())
 
 	default:
-		klog.V(5).InfoS("Ignore packet coming from frontend",
-			"type", pkt.Type, "serverID", s.serverID, "connectionID", firstConnID)
+		t.logger.V(5).Info("Ignore unknown packet coming from frontend",
+			"type", pkt.Type.String(), "dialID", t.dialID(), "connectID", t.connectID())
 	}
 }
 
 func (t *grpcTunnel) RecvBackend(pkt *client.Packet) {
-	// TODO
+	switch pkt.Type {
+	case client.PacketType_DIAL_RSP:
+		if s := t.getState(); s != TunnelDialing {
+			t.logger.Error(nil, fmt.Sprintf("DIAL_RSP received in state %q", s.String()))
+			// How we handle this situation depends on whether it was an error response.
+		}
+		rsp := pkt.GetDialResponse()
+		if t.connectID() == 0 && rsp.ConnectID != 0 {
+			t.setConnectID(rsp.ConnectID)
+		} else if t.connectID() != rsp.ConnectID {
+			t.logger.Error(nil, "Received DIAL_RSP with mismatched connection ID",
+				"state", t.getState(),
+				"tunnelConnectID", t.connectID(),
+				"responseConnectID", rsp.ConnectID,
+				"agentID", t.agentID)
+			// Something went very wrong here. Best to just shutdown.
+			t.setState(TunnelShutdown)
+			t.backend.close()
+			t.frontend.close()
+			return
+		}
+
+		if rsp.Error != "" {
+			t.handleDialError(pkt)
+		} else {
+			t.handleDialSuccess(pkt)
+		}
+
+	case client.PacketType_DIAL_CLS:
+		if s := t.getState(); s != TunnelDialing {
+			t.logger.Error(nil, "Received DIAL_CLS from backend in unexpected state. Proceeding to shutdown anyway.",
+				"state", s,
+				"dialID", t.dialID(),
+				"agentID", t.agentID)
+		}
+		t.setState(TunnelShutdown)
+		t.backend.setState(ConnectionClosing)
+		t.frontend.close()
+
+	case client.PacketType_CLOSE_RSP:
+		if s := t.getState(); s != TunnelConnected {
+			t.logger.Error(nil, "Received CLOSE_RSP from backend in unexpected state. Proceeding to shutdown anyway.",
+				"state", s,
+				"connectID", t.connectID(),
+				"agentID", t.agentID)
+		}
+		t.setState(TunnelShutdown)
+		t.backend.setState(ConnectionClosing)
+		t.frontend.setState(ConnectionClosing)
+		if err := t.frontend.Send(pkt); err != nil {
+			t.logger.Error(err, "Failed to deliver CLOSE_RSP to frontend",
+				"connectID", t.connectID())
+		}
+
+	case client.PacketType_DATA:
+		if s := t.getState(); s != TunnelConnected {
+			t.logger.V(5).Info("Ignoring DATA packet from backend: invalid state.",
+				"state", s,
+				"connectID", t.connectID())
+			return
+		}
+		if err := t.frontend.Send(pkt); err != nil {
+			t.logger.Error(err, "Failed to deliver DATA packet to frontend",
+				"connectID", t.connectID(),
+				"agentID", t.agentID)
+		}
+
+	case client.PacketType_DIAL_REQ, client.PacketType_CLOSE_REQ:
+		t.logger.V(2).Info("Invalid packet from backend", "type", pkt.Type.String())
+
+	default:
+		t.logger.V(5).Info("Ignore unknown packet coming from backend",
+			"type", pkt.Type.String(), "dialID", t.dialID(), "connectID", t.connectID(), "agentID", t.agentID)
+	}
+}
+
+func (t *grpcTunnel) handleDialError(pkt *client.Packet) {
+	t.setState(TunnelShutdown)
+
+	rsp := pkt.GetDialResponse()
+	t.logger.Error(errors.New(rsp.Error), "DIAL_RSP contains failure", "dialID", t.dialID(), "agentID", t.agentID)
+	t.backend.setState(ConnectionClosing)
+
+	if t.frontend.getState() == ConnectionClosing {
+		// Frontend is already shutting down, nothing more to do.
+		return
+	}
+
+	if err := t.frontend.Send(pkt); err != nil {
+		t.logger.Error(err, "DIAL_RSP send to frontend stream failure", "dialID", t.dialID())
+		t.frontend.close() // Last ditch effort to close the frontend.
+	}
+	t.frontend.setState(ConnectionClosing)
+}
+
+func (t *grpcTunnel) handleDialSuccess(pkt *client.Packet) {
+	t.setState(TunnelConnected)
+	t.backend.setState(ConnectionEstablished)
+
+	if err := t.frontend.Send(pkt); err != nil {
+		t.logger.Error(err, "DIAL_RSP send to frontend stream failure", "dialID", t.dialID())
+		// Failed to establish the connection, so we need to shutdown.
+		t.setState(TunnelShutdown)
+		t.frontend.close() // Last ditch effort to close the frontend.
+		t.backend.close()
+		return
+	}
+	t.frontend.setState(ConnectionEstablished)
 }
 
 func (t *grpcTunnel) FrontendClosed() {
@@ -209,6 +315,12 @@ func (t *grpcTunnel) BackendClosed() {
 
 	t.backend.setState(ConnectionClosed)
 	t.frontend.close()
+}
+
+func (t *grpcTunnel) getState() TunnelState {
+	t.stateMu.Lock()
+	defer t.stateMu.Unlock()
+	return t.state
 }
 
 func (t *grpcTunnel) setState(s TunnelState) {
@@ -231,7 +343,7 @@ type connection struct {
 	GrpcClient
 
 	mu     sync.Mutex
-	logger logr.Logger
+	logger klog.Logger
 	state  ConnectionState
 	id     connID
 }
@@ -240,6 +352,12 @@ func (c *connection) setState(s ConnectionState) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.state = s
+}
+
+func (c *connection) getState() ConnectionState {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.state
 }
 
 func (c *connection) close() {
@@ -258,6 +376,7 @@ func (c *connection) close() {
 			},
 		}
 	case ConnectionEstablished:
+		// FIXME - CLOSE_REQ for backend, CLOSE_RSP for frontend :(
 		closeReq = &client.Packet{
 			Type: client.PacketType_CLOSE_REQ,
 			Payload: &client.Packet_CloseRequest{
@@ -274,7 +393,7 @@ func (c *connection) close() {
 		return
 	}
 
-	if err := c.client.Send(closeReq); err != nil {
+	if err := c.Send(closeReq); err != nil {
 		c.logger.Error(err, "Failed to deliver close request",
 			"type", closeReq.Type,
 			"dialID", c.id.dialID(),
